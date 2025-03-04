04f608b73d2dd54ed20528dca605bbc4320e56df
LehMipo412 <barbetzalel412@gmail.com>
2025-03-04 19:57:13 +0200

    added basic painter shader that works poorly but its a progress

--- BarberShaderProject/Assets/Scripts/ManipulateMatirialFloatTest.cs
+++ BarberShaderProject/Assets/Scripts/ManipulateMatirialFloatTest.cs
@@ -6,15 +6,33 @@ public class ManipulateMatirialFloatTest : MonoBehaviour
 {
 
    
-    public GameObject testeSphere;
-    public Material instanceMaterial; 
+    //public GameObject testeSphere;
+    //public Material instanceMaterial; 
     public RaycastHit hitInfo;
+
+    [SerializeField] private Shader _drawShader;
+    private RenderTexture splatMap;
+    private Material _drawMaterial, _currentMaterial;
+
+    [SerializeField] [Range(1, 500)] private float size;
+    [SerializeField] [Range(0,1)] private float strength;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        _drawMaterial = new Material(_drawShader);
+        _drawMaterial.SetVector("_Color", Color.red);
+        _currentMaterial = GetComponent<Renderer>().material;
+        splatMap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        _currentMaterial.SetTexture("_SplatMap", splatMap);
+
+
+
         //instanceMaterial = testMatirial;
        //instanceMaterial = testeSphere.GetComponent<Renderer>().material; 
        // instanceMaterial.SetFloat("_ColorParameter", 0.5f);
+
 
         
     }
@@ -29,8 +47,18 @@ public class ManipulateMatirialFloatTest : MonoBehaviour
             if (Physics.Raycast(ray, out hithit, 100))
             {
                 print("Hit something! " + hithit.collider.name);
+                // hithit.collider.gameObject.GetComponent<Renderer>().material.SetVector("_HitLocation", hithit.point);
 
-                hithit.collider.gameObject.GetComponent<Renderer>().material.SetVector("_HitLocation", hithit.point);
+                _drawMaterial.SetVector("_Coordinates", new Vector4(hithit.textureCoord.x, hithit.textureCoord.y, 0, 0));
+                _drawMaterial.SetFloat("_Strength", strength);
+                _drawMaterial.SetFloat("_Size", size);
+
+
+                RenderTexture temp = RenderTexture.GetTemporary(splatMap.width,splatMap.height,0,RenderTextureFormat.ARGBFloat);
+                Graphics.Blit(splatMap, temp);
+                Graphics.Blit(temp, splatMap, _drawMaterial);
+                RenderTexture.ReleaseTemporary(temp);
+
 
                // instanceMaterial.SetVector("_HitLocation", hithit.point);
 
